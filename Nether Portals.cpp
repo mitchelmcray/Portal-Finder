68523d299c148dcfc8a4b7f86f9caad38b6c2d98
mitchelmcray <mitchel.mcray@my.wheaton.edu>
2021-02-11 20:39:19 -0600

    Add get_all_coords function
    Added the initial implimentation of get_all_coords which generates a set
    of coordinate tuples comprising all valid 3D locations in an integer
    coordinate space within the given distance.
    
    The function test_coordinate_set has been started, but not fully
    implemented at this time.

--- Nether Portals.cpp
+++ Nether Portals.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 //function declarations
+set<tuple<int,int,int>> get_all_coords(tuple<int,int,int> coordinate, int distance);
 tuple<tuple<int, int>, tuple<int, int>, tuple<int, int>> get_constraints(
     tuple<int, int, int> coordinates, int distance);
 tuple<int, int> get_range(int coordinate, int distance);
@@ -43,8 +44,32 @@ int main()
     tie(z_left_bound, z_right_bound) = z_bounds;
     cout<<"z bounds:\r\n" << z_left_bound << " " << z_right_bound << "\r\n";
 
+    set<tuple<int, int, int>> test_set = get_all_coords(test_coords, distance);
+
     return 0;
 }
+
+/**
+ * 
+ *
+int test_coordinate_set(set<tuple<int, int ,int>> coordinate_set, int distance){
+    int set_size = coordinate_set.size();
+    int expected_size = distance*distance*distance;
+    if(!(set_size==expected_size)){
+        if(set_size<expected_size){
+            cout << "ERROR: Set has fewer elements than expected";
+        } else {
+            cout << "ERROR: Set has more elements than expected";
+        }
+    }
+
+    set<tuple<int, int, int>>::iterator coord_it = coordinate_set.begin();
+    set<tuple<int, int, int>>::iterator end_it = coordinate_set.end();
+    while(coord_it != end_it){
+        check_distance(*it);
+    }
+}*/
+
 
 /**
  * Finds all coordinates within a given distance.
@@ -57,7 +82,58 @@ int main()
  * @return A set of tuples containing all the 3D coordinates
  */
 set<tuple<int,int,int>> get_all_coords(tuple<int,int,int> coordinate, int distance){
+    //Declare return value and initial control variables
     set<tuple<int, int, int>> to_return;
+    int x, y, z, y_lower, y_upper;
+    //Decompose coordinate into int values
+    tie(x, y, z) = coordinate;
+    //Get the lower and upper bounds of y-coordinate for given distance
+    tie(y_lower, y_upper) = get_range(y, distance); //y_upper is currently unused
+    
+    //Iterate over each integer value between y_lower and y_upper (y_upper = y_lower + 2*distance)
+    for(int i=0; i<=(2*distance);i++){
+        //Calculate actual y-coordinate value for the current iteration
+        int current_y = y_lower +i;
+        //Calculate the current upper bound on the absoulte value for the x-coordinate
+        int j_bound = distance-abs(distance-i);
+
+        //Iterate over the absolute values for the distances of the current possible x-coordinates
+        for(int j=0; j<=j_bound;j++){
+            //Declare and initialize control variables related to the x-coordinate
+            int min_x, plus_x, k_bound = j_bound-j; //determine the bound on the z-coordinate from the x
+            //Calculate the actual x-coordinate values from the current distance
+            min_x = x-j;
+            plus_x = x+j;
+
+            //Iterate over the absolute values for the distances of the current possible z-coordinates
+            for(int k=0; k<=k_bound;k++){
+                //Declare and initialize control variables related to the z-coordinate
+                int min_z, plus_z;
+                min_z = z-k;
+                plus_z = z+k;
+
+                //Insert all possible coordinates for the current constraints
+                if(!j){ //Check if current x-coordinate distance is 0
+                    if(!k){ //Check if current z-coordinate distance is 0
+                        to_return.insert(make_tuple(x, current_y, z));
+                    } else {
+                        to_return.insert(make_tuple(x, current_y, min_z));
+                        to_return.insert(make_tuple(x, current_y, plus_z));
+                    }
+                } else { //Check if current z-coordinate distance is 0
+                     if(!k){
+                        to_return.insert(make_tuple(min_x, y, z));
+                        to_return.insert(make_tuple(plus_x, y, z));
+                    } else{
+                        to_return.insert(make_tuple(min_x, y, min_z));
+                        to_return.insert(make_tuple(min_x, y, plus_z));
+                        to_return.insert(make_tuple(plus_x, y, min_z));
+                        to_return.insert(make_tuple(plus_x, y, plus_z));
+                    }
+                }
+            }
+        }
+    }
     return to_return;
 }
 
